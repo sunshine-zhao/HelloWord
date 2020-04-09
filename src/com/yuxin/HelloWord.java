cb9f137cefebf7ae2964b05fce6840f45dc7c34c
zhaoyao <47496838@qq.com>
2020-04-09 15:48:57 +0800

    Hello2020/04/09/01

--- src/com/yuxin/HelloWord.java
+++ src/com/yuxin/HelloWord.java
@@ -2,7 +2,7 @@ package com.yuxin;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello  World!");
+        System.out.println("Hello  Worrld!");
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 //        Thread.currentThread().setContextClassLoader();
     }
